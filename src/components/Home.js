fbe9f2be48f1bc8319272bb2318340fcbbe16eed
Ankit Kumar <ankit.kr0048@gmail.com>
2021-06-01 19:45:51 +0530

    Updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import Illustration from '../images/illustration/message.svg';
-
+import {Link} from 'react-router-dom';
 const Home = () => {
     return (
-        <div className="bg-blue-100">
-            <div className="grid grid-cols-2 gap-4 justify-items-center ">
-                <div className="p-10">
-                    <h1 className="font-extrabold leading-tight text-7xl">Welcome<br/>To<br/>Skybox.</h1>
-                </div>
-                <div className="">
-                <img src={Illustration} alt="register" className="h-5/6 mt-10 pr-36"/>
-                </div>
-            </div>
+        <div className="bg-blue-100 text-center mx:auto p-20">
+            <h1 className="font-extrabold leading-tight text-9xl mb-20">Welcome To<br/>Skybox.</h1>
+                
+            <Link to="./login" className="p-4 pl-12 pr-12 bg-blue-400 text-2xl">Start Chatting!</Link>
         </div>
     )
 }
